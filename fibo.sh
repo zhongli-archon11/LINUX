958b743a826f3e8c9a928c9896af2c97f0221d3a
Rex Lapis <91016552+zhongli-archon11@users.noreply.github.com>
2023-02-25 11:25:41 +0530

    Fibonacci series

---
+++ fibo.sh
@@ -0,0 +1,21 @@
+#shell script to find n Fibonacci numbers
+
+#!/bin/bash
+
+echo "How many numbers do you want of Fibonacci series ?"
+read total
+x=0
+y=1
+i=2
+echo "Fibonacci Series up to $total terms :: "
+echo "$x"
+echo "$y"
+while [ $i -lt $total ]
+do
+i=`expr $i + 1 `
+z=`expr $x + $y `
+echo "$z"
+x=$y
+y=$z
+done
+
