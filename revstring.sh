dafda784f4ab08dfb8f85bad8db60aef124cee52
Rex Lapis <91016552+zhongli-archon11@users.noreply.github.com>
2023-02-25 11:31:43 +0530

    reverse string

---
+++ revstring.sh
@@ -0,0 +1,13 @@
+#shell script to find the reverse of a string
+
+#!/bin/bash
+read -p "Enter a string: " str
+length=${#str}
+i=$((length-1))
+while [ $i -ge 0 ]
+do
+	revstr=$revstr${str:$i:1}
+	i=$((i-1))
+done
+echo "Reverse of $str : $revstr"
+
