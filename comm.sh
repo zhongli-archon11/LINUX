c3f1db8b1b7dfa5f920f3bb54f4ef2ee3e92270a
Rex Lapis <91016552+zhongli-archon11@users.noreply.github.com>
2023-02-25 11:17:27 +0530

    comments types

---
+++ comm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Multi line comment"
+
+echo "Using <<comment .... comment"
+<<comm
+	Code or Comment
+comm
+
+echo "Using :' ... '"
+: '
+	Code or Comment
+ '
+ 
+ echo "Single line comment"
+
+echo "Using #"
+ #Code or Comment
