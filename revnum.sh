f12b421860af5a8979498f1eec59ac4bd8681615
Rex Lapis <91016552+zhongli-archon11@users.noreply.github.com>
2023-02-25 11:31:06 +0530

    Reverse number

---
+++ revnum.sh
@@ -0,0 +1,15 @@
+#shell script to find the reverse of a number
+
+#!/bin/bash
+
+echo "Enter a number="
+read n
+num=0
+while [ $n -gt 0 ]
+do
+num=$(expr $num \* 10)
+k=$(expr $n % 10)
+num=$(expr $num + $k)
+n=$(expr $n / 10)
+done
+echo "Reverse=" $num
