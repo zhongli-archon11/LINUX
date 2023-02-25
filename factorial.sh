0d568a02a332dee5080d063aff255554e6522e74
Rex Lapis <91016552+zhongli-archon11@users.noreply.github.com>
2023-02-25 11:21:41 +0530

    Factorial

---
+++ factorial.sh
@@ -0,0 +1,15 @@
+#shell script to find the factorial of a number
+
+#!/bin/bash
+echo "Enter a number"
+read num
+
+fact=1
+
+while [ $num -gt 1 ]
+do
+  fact=$((fact * num))  #fact = fact * num
+  num=$((num - 1))      #num = num - 1
+done
+
+echo $fact
